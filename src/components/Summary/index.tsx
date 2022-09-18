d44140e69274ccfd520bb304823b625a067196ca
João Debussy <social.debussy@gmail.com>
2022-09-18 10:09:11 -0300

    build: add miragejs dependecy

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -19,14 +19,14 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcome} alt="Saídas" />
         </header>
-        <strong>- R$ 5000,00</strong>
+        <strong>- R$ 500,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={total} alt="Total" />
         </header>
-        <strong>R$ 5000,00</strong>
+        <strong>R$ 500,00</strong>
       </div>
     </Container>
   )
